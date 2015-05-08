0db9245bb072468f81549fca1564283d97e0e599
Jan Miksovsky <jan@quickui.org>
2015-05-08 11:49:18 -0700

    Convert BasicGenericHelper to a Polymer behavior.

--- src/basic-shared/basic-helpers/BasicGenericHelpers.js
+++ src/basic-shared/basic-helpers/BasicGenericHelpers.js
@@ -9,51 +9,38 @@
  * added to the default style won't be overridden by default, making it hard to
  * know whether a new version of a component will still look okay.
  *
- * As a compromise, these helpers allow the creation of a "generic" attribute.
- * This attribute is normally set by default, and styles can be written that
- * apply only when the generic attribute is set. This makes it easy to remove
- * all default styling -- set the generic attribute to false, and all default
- * styling will be removed.
+ * As a compromise, the simple Polymer behavior here defines a "generic"
+ * attribute. This attribute is normally set by default, and styles can be
+ * written that apply only when the generic attribute is set. This allows the
+ * construction of CSS rules that will only apply to generic components like
+ *
+ *     :host([generic=""]) {
+ *       ...
+ *     }
+ *
+ * This makes it easy to remove all default styling -- set the generic attribute
+ * to false, and all default styling will be removed.
+ *
  */
 
-BasicGenericHelpers = {
+BasicGenericBehavior = {
 
-  /*
-   * This member can be included in the "properties" key for a Polymer element.
-   * E.g.:
-   *
-   * Polymer({
-   *   is: 'my-element',
-   *   properties: {
-   *     generic: BasicGenericHelpers.generic
-   *   }
-   * });
-   *
-   */
-  generic: {
-    type: Boolean,
-    value: null,
-    reflectToAttribute: true
-  },
-
-  /*
-   * Invoke this method in a component's ready handler to set the generic
-   * attribute to true by default.
-   *
-   * We explicitly set the generic attribute to true, rather than providing that
-   * as a default value. A default value won't appear on an element instance as
-   * an attribute, but an attribute we set explicitly will. We need the
-   * attribute there so that generic styling can be set simply by defining rules
-   * like
-   *
-   *  :host([generic=""]) {
-   *    ...
-   *  }
-   *
-   */
-  setGenericDefault: function(element) {
-    if (element.getAttribute('generic') == null) {
-      element.generic = true;
+  properties: {
+    /**
+     * True if the component would like to receive generic styling.
+     *
+     * This property is true by default — set it to false to turn off all
+     * generic styles. This makes it easier to apply custom styling; you won't
+     * have to explicitly override styling you don't want.
+     *
+     * @property generic
+     * @type Boolean
+     * @default true
+     */
+    generic: {
+      type: Boolean,
+      value: true,
+      reflectToAttribute: true
     }
   }
 
