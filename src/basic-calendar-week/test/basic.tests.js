2dd070e661e1411002ba81c3edfdc5ee46eab292
Rob Bearman <robbear@hyperfine.com>
2015-07-13 11:40:29 -0700

    More work on basic-calendar-week. Unit tests work. Globalize helpers such as dateFirstDayOfWeek are scoped inaccessible, so it's not clear how we access culture info.

--- src/basic-calendar-week/test/basic.tests.js
+++ src/basic-calendar-week/test/basic.tests.js
@@ -13,10 +13,12 @@ suite('basic', function() {
     var fixture = document.createElement('basic-calendar-week');
     container.appendChild(fixture);
     var now = new Date();
-    assert.equal(now.getDay(), fixture.date.getDay());
-    assert.equal(now.getMonth(), fixture.date.getMonth());
-    assert.equal(now.getFullYear(), fixture.date.getFullYear());
-    done();
+    flush(function() {
+      assert.equal(now.getDay(), fixture.date.getDay());
+      assert.equal(now.getMonth(), fixture.date.getMonth());
+      assert.equal(now.getFullYear(), fixture.date.getFullYear());
+      done();
+    });
   });
 
   test('set date attribute', function (done) {
