2cb4920420cb78ebb5a826ff8c10de79d115ddcb
Jan Miksovsky <jan@quickui.org>
2015-10-05 16:41:41 -0700

    Filter out auxiliary elements: link, script, style, and template.

--- src/basic-content-items/test/basic-content-items.tests.js
+++ src/basic-content-items/test/basic-content-items.tests.js
@@ -17,6 +17,21 @@ suite('basic-content-items', function() {
     assert.deepEqual(items, simpleContent.data);
   });
 
+  test("auxiliary elements filtered from items", function() {
+    var collective = new Basic.Collective();
+    var simpleContent = clone(simpleContentAspect);
+    var div = document.createElement('div');
+    var link = document.createElement('link');
+    var script = document.createElement('script');
+    var style = document.createElement('style');
+    var template = document.createElement('template');
+    simpleContent.data = [div, link, script, style, template ];
+    collective.assimilate(simpleContent, clone(Basic.ContentItems));
+    var items = collective.items;
+    assert.equal(items.length, 1);
+    assert.equal(items[0], div);
+  });
+
 });
 
 function clone(obj) {
