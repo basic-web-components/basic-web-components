2d59f6e97f610d320e78dcc560b22ce29e9ac5b5
Rob Bearman <robbear@hyperfine.com>
2016-01-08 10:53:29 -0800

    Clean up Gruntfile.js, removing old cruft. Eliminated unused npm modules in root package.json. Refreshed npm-shrinkwrap.json.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,138 +1,14 @@
-var SRC_DIR = 'src';
-var GRUNT_TEST_DIR = 'grunt/test';
-var ROOT_DIR = './';
-var TEMPLATE_DIR = 'grunt/templates';
 
 module.exports = function(grunt) {
 
-  grunt.loadNpmTasks('grunt-banner');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-hogan-static');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-text-replace');
-
-  var modules = grunt.file.expand({cwd: SRC_DIR}, 'basic-*');
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    src_dir: 'src',
-    build_dir: 'build',
-    dist_dir: 'dist',
 
     clean: {
-      build: '<%= build_dir %>',
-      dist: '<%= dist_dir %>'
-    },
-
-    copy: {
-      build: {
-        expand: true,
-        cwd: SRC_DIR,
-        src: [
-          'basic-*/+(basic-*).+(html|js)',
-          'basic-shared/*.js',
-          'basic-shared/resources/**',
-          '!test/**'],
-        dest: '<%= build_dir %>'
-      },
-      dist: {
-        expand: true,
-        cwd: '<%= build_dir %>',
-        src: ['**/*.html', '**/*.js'],
-        dest: '<%= dist_dir %>'
-      },
-      test: {
-        expand: true,
-        cwd: GRUNT_TEST_DIR,
-        src: ['index.html'],
-        dest: '<%= dist_dir %>'
-      },
-      remote_test: {
-        expand: true,
-        cwd: GRUNT_TEST_DIR,
-        src: ['index.html'],
-        dest: '<%= dist_dir %>',
-        rename: function(dest, src) {
-          return dest + '/remote-test.html';
-        }
-      },
-      bower_dist: {
-        expand: true,
-        cwd: ROOT_DIR,
-        src: ['bower_components/**'],
-        dest: '<%= dist_dir %>'
-      }
-    },
-
-    hogan_static: {
-      lib: {
-        options:{
-          data: {
-            modules: modules,
-            'basic-shared': grunt.file.expand({cwd: SRC_DIR})
-          }
-        },
-        files: [
-          {
-            src: TEMPLATE_DIR + '/lib_template.html',
-            dest: '<%= build_dir %>/<%= pkg.name %>.html'
-          },
-          {
-            src: TEMPLATE_DIR + '/lib_template.html',
-            dest: '<%= build_dir %>/<%= pkg.name %>-polymer.html'
-          }
-        ]
-      }
-    },
-
-    replace: {
-      bower: {
-        src: ['dist/**/*.html'],
-        overwrite: true,
-        replacements: [{
-          from: 'bower_components',
-          to: '..'
-        }]
-      },
-      cldr: {
-        src: ['dist/basic-web-components.html'],
-        overwrite: true,
-        replacements: [{
-          from: '../../../cldr-data/',
-          to: '../bower_components/cldr-data/'
-        }]
-      },
-      remote_test: {
-        src: ['dist/remote-test.html'],
-        overwrite: true,
-        replacements: [{
-          from: 'basic-web-components.html',
-          to: 'http://hosting.component.kitchen/bwc/v0.6.0/basic-web-components.html'
-        }]
-      }
-    },
-
-    banner: grunt.file.read('BANNER.txt'),
-
-    usebanner: {
-      dist: {
-        options: {
-          banner: '<!--\n<%= banner %>\n-->'
-        },
-        files: {
-          src: [ 'build/basic-web-components.html' ]
-        }
-      },
-      dist_include_polymer: {
-        options: {
-          banner: '<!--\n<%= banner %>\n-->'
-        },
-        files: {
-          src: [ 'build/basic-web-components-polymer.html' ]
-        }
-      }
+      build: 'build'
     },
 
     browserify: {
