bb5ac757e9023da661ff17086e735297eff6ec29
Rob Bearman <robbear@hyperfine.com>
2016-01-08 10:42:07 -0800

    Remove duplicate load of grunt-browserify in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,6 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-banner');
   grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-hogan-static');
