c7363c708c359e00949c8b12bbade3c397cb6361
Rob Bearman <robbear@hyperfine.com>
2016-01-11 11:22:44 -0800

    Added npm-publish support. Added grunt-shell to package.json and updated npm-shrinkwrap.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,49 @@
  * Note: this file is in ES5. The rest of this project is ES6.
  */
 
+//
+// allPackages is the array of npm-publishable packages in this monorepo
+//
+var allPackages = [
+  'basic-arrow-selection',
+  'basic-autosize-textarea',
+  'basic-carousel',
+  'basic-component-mixins',
+  'basic-element-base',
+  'basic-list-box',
+  'basic-page-dots',
+  'basic-sliding-viewport',
+  'basic-spread-items'
+];
+
+//
+// Build the buildFiles object for use in browserify:components
+//
+function buildBuildFilesObject() {
+  var obj = {'build/basic-web-components.js': ['packages/*/src/*.js']};
+
+  for (var i = 0; i < allPackages.length; i++) {
+    obj['packages/' + allPackages[i] + '/dist/' + allPackages[i] + '.js'] = ['packages/' + allPackages[i] + '/*.js'];
+  }
+
+  return obj;
+}
+var buildFiles = buildBuildFilesObject();
+
+//
+// Watch files - same as buildFiles with the following additions
+//
+var watchFiles = buildFiles;
+watchFiles['build/tests.js'] = ['packages/*/test/*.js'];
+
+
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-shell');
 
   grunt.initConfig({
 
@@ -26,18 +63,7 @@ module.exports = function(grunt) {
         transform: ['babelify']
       },
       components: {
-        files: {
-          'build/basic-web-components.js': ['packages/*/src/*.js'],
-          'packages/basic-arrow-selection/dist/basic-arrow-selection.js': ['packages/basic-arrow-selection/src/*.js'],
-          'packages/basic-autosize-textarea/dist/basic-autosize-textarea.js': ['packages/basic-autosize-textarea/src/*.js'],
-          'packages/basic-carousel/dist/basic-carousel.js': ['packages/basic-carousel/src/*.js'],
-          'packages/basic-component-mixins/dist/basic-component-mixins.js': ['packages/basic-component-mixins/*.js'],
-          'packages/basic-element-base/dist/basic-element-base.js': ['packages/basic-element-base/src/*.js'],
-          'packages/basic-list-box/dist/basic-list-box.js': ['packages/basic-list-box/src/*.js'],
-          'packages/basic-page-dots/dist/basic-page-dots.js': ['packages/basic-page-dots/src/*.js'],
-          'packages/basic-sliding-viewport/dist/basic-sliding-viewport.js': ['packages/basic-sliding-viewport/src/*.js'],
-          'packages/basic-spread-items/dist/basic-spread-items.js': ['packages/basic-spread-items/src/*.js']
-        }
+        files: buildFiles
       },
       test: {
         files: {
@@ -45,19 +71,7 @@ module.exports = function(grunt) {
         }
       },
       watch: {
-        files: {
-          'build/basic-web-components.js': ['packages/*/src/*.js'],
-          'packages/basic-arrow-selection/dist/basic-arrow-selection.js': ['packages/basic-arrow-selection/src/*.js'],
-          'packages/basic-autosize-textarea/dist/basic-autosize-textarea.js': ['packages/basic-autosize-textarea/src/*.js'],
-          'packages/basic-carousel/dist/basic-carousel.js': ['packages/basic-carousel/src/*.js'],
-          'packages/basic-component-mixins/dist/basic-component-mixins.js': ['packages/basic-component-mixins/*.js'],
-          'packages/basic-element-base/dist/basic-element-base.js': ['packages/basic-element-base/src/*.js'],
-          'packages/basic-list-box/dist/basic-list-box.js': ['packages/basic-list-box/src/*.js'],
-          'packages/basic-page-dots/dist/basic-page-dots.js': ['packages/basic-page-dots/src/*.js'],
-          'packages/basic-sliding-viewport/dist/basic-sliding-viewport.js': ['packages/basic-sliding-viewport/src/*.js'],
-          'packages/basic-spread-items/dist/basic-spread-items.js': ['packages/basic-spread-items/src/*.js'],
-          'build/tests.js': 'packages/*/test/*.js'
-        },
+        files: watchFiles,
         options: {
           keepAlive: true,
           watch: true
@@ -69,7 +83,7 @@ module.exports = function(grunt) {
       all: [
         'packages/**/*.js',
         '!packages/**/dist/*',
-        'test/**/*.js',
+        'test/**/*.js'
       ],
       options: {
         jshintrc: true
@@ -90,6 +104,56 @@ module.exports = function(grunt) {
           run: true
         }
       }
+    },
+
+    shell: {
+      'npm-publish': {
+        command: function(package) {
+          if (!package || package.length < 1) {
+            return '';
+          }
+          return 'npm publish packages/' + package;
+        },
+        options: {
+          stderr: false,
+          stdout: false,
+          callback: function(err, stdout, stderr, cb) {
+            if (err && err.toString().indexOf('E403') < 0) {
+                grunt.log.error('Package not published for unexpected reasons: ' + err);
+            }
+            else if (stdout && stdout.length > 0) {
+              grunt.log.writeln(stdout);
+            }
+            if (cb) {
+              cb();
+            }
+          }
+        }
+      },
+
+      'npm-owner-add': {
+        command: function(owner, package) {
+          if (!package || package.length < 1 || !owner || owner.length < 1) {
+            return '';
+          }
+          return 'npm owner add ' + owner + ' ' + package;
+        },
+        options: {
+          stderr: false,
+          stdout: false,
+          callback: function(err, stdout, stderr, cb) {
+            if (err && err.toString().indexOf('E403') < 0) {
+              grunt.log.error('Package owner not updated for unexpected reasons: ' + err);
+            }
+            else if (stdout && stdout.length > 0) {
+              grunt.log.writeln(stdout);
+            }
+            if (cb) {
+              cb();
+            }
+          }
+        }
+      }
     }
 
   });
@@ -98,6 +162,7 @@ module.exports = function(grunt) {
     grunt.log.writeln('grunt commands this project supports:\n');
     grunt.log.writeln('  grunt build (builds consolidated basic-web-components.js, all package distributions, and all tests)');
     grunt.log.writeln('  grunt lint (runs jshint on all .js files)');
+    grunt.log.writeln('  grunt npm-publish:package-name|* (publishes packages/package-name or all packages (packages/*) to npm)');
     grunt.log.writeln('  grunt watch (builds and watches changes to project files)');
   });
 
@@ -105,5 +170,34 @@ module.exports = function(grunt) {
   grunt.registerTask('lint', ['jshint']);
   grunt.registerTask('test', ['mocha']);
   grunt.registerTask('watch', ['browserify:watch']);
+
+  grunt.registerTask('npm-addowners', function(package) {
+    if (!package || package.length < 1) {
+      return grunt.log.error('No package specified');
+    }
+
+    grunt.task.run([
+      'shell:npm-owner-add:jan.miksovsky:' + package,
+      'shell:npm-owner-add:robbear:' + package]);
+  });
+
+  grunt.registerTask('npm-publish', function(package) {
+    if (!package || package.length < 1) {
+      return grunt.log.error('No package specified');
+    }
+
+    var packages = [];
+
+    if (package == '*') {
+      packages = allPackages;
+    }
+    else {
+      packages.push(package);
+    }
+
+    for (var i = 0; i < packages.length; i++) {
+      grunt.task.run(['shell:npm-publish:' + packages[i], 'npm-addowners:' + packages[i]]);
+    }
+  });
 
 };
