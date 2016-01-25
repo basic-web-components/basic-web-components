ab8a479013cd48b1539cae03bd95912fb3028b19
Rob Bearman <robbear@hyperfine.com>
2016-01-25 08:55:33 -0800

    Added grunt-saucelabs and grunt-contrib-connect

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,6 +84,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-jsdoc-to-markdown');
   grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-saucelabs');
   grunt.loadNpmTasks('grunt-shell');
 
   grunt.initConfig({
