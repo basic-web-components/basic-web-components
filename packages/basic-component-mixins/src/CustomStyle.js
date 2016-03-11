ba7811ee7a57211638661ab4dd0fc3a8efca075e
Jan Miksovsky <jan@quickui.org>
2016-03-11 15:20:22 -0800

    Add comments.

--- packages/basic-component-mixins/src/CustomStyle.js
+++ packages/basic-component-mixins/src/CustomStyle.js
@@ -1,20 +1,33 @@
 export default (base) => {
 
+  /**
+   * Experimental mixin for custom styling via an inline attribute.
+   */
   class CustomStyle extends base {
 
     get customStyle() {
       return this._customStyle;
     }
     set customStyle(customStyle) {
+
+      if ('customStyle' in base) { super.customStyle = customStyle; }
+
+      // Find the existing style element.
       let styleElement = this.shadowRoot.querySelector('style');
       if (!styleElement) {
-        console.warn("Tried to set custom-style on an element whose shadow tree contains no <style> element.");
-        return;
+        // Create a new style element.
+        styleElement = document.createElement('style');
+        this.shadowRoot.appendChild(styleElement);
       }
       if (!this._originalStyle) {
         this._originalStyle = styleElement.textContent;
       }
+
+      // Remember just the custom portion of the style so we can serve it back
+      // if requested.
       this._customStyle = customStyle;
+
+      // Append the custom style to the existing style.
       styleElement.textContent = `${this._originalStyle}\n${customStyle}`;
     }
 
