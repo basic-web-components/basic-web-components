126d906237a09d22662fd01519e42e89168ede6b
Jan Miksovsky <jan@quickui.org>
2016-04-07 14:53:18 -0700

    Some initial success animating selection.

--- packages/basic-animation-stage/src/AnimationStage.js
+++ packages/basic-animation-stage/src/AnimationStage.js
@@ -35,77 +35,57 @@ class AnimationStage extends base {
     return 200;
   }
 
-  applySelection(item, selected) {
-    let index = this.items.indexOf(item);
-    let selectedIndex = this.selectedIndex; // Too expensive?
-    let fraction;
-    if (selected) {
-      fraction = 0.5;
-    } else {
-      fraction = index > selectedIndex ?
-        0 :
-        1;
-    }
-    console.log(`${item.textContent} selected: ${selected}, index: ${index} (current: ${selectedIndex}), ${fraction}`);
-    animateItem(this, item, fraction);
+
+  // position is between -1 (previous) and 1 (next). 0 = current item.
+  animateItem(item, startPosition, endPosition) {
+    let animation = [
+      this.animation[startPosition + 1],
+      this.animation[endPosition + 1]
+    ];
+    console.log(animation);
+    let animationOptions = {
+      duration: this.animationDuration,
+      easing: 'ease-in-out',
+      fill: 'both'
+    };
+    item.animate(animation, animationOptions);
   }
+
+  // applySelection(item, selected) {
+  //   let index = this.items.indexOf(item);
+  //   let selectedIndex = this.selectedIndex; // Too expensive?
+  //   let fraction;
+  //   if (selected) {
+  //     fraction = 0.5;
+  //   } else {
+  //     fraction = index > selectedIndex ?
+  //       0 :
+  //       1;
+  //   }
+  //   console.log(`${item.textContent} selected: ${selected}, index: ${index} (current: ${selectedIndex}), ${fraction}`);
+  //   animateItem(this, item, fraction);
+  // }
 
   attachedCallback() {
     if (super.attachedCallback) { super.attachedCallback(); }
     this.selectionRequired = true;
   }
+  //
+  // itemsChanged() {
+  //   console.log("itemsChanged");
+  //   console.log("Resetting players");
+  //   this._players = null;
+  //   if (super.itemsChanged) { super.itemsChanged(); }
+  // }
+  //
+  // get players() {
+  //   if (!this._players && this.items) {
+  //     console.log("Creating players");
+  //     this._players = new Array(this.items.length);
+  //   }
+  //   return this._players;
+  // }
 
-  itemsChanged() {
-    console.log("itemsChanged");
-    console.log("Resetting players");
-    this._players = null;
-    if (super.itemsChanged) { super.itemsChanged(); }
-  }
-
-  get players() {
-    if (!this._players && this.items) {
-      console.log("Creating players");
-      this._players = new Array(this.items.length);
-    }
-    return this._players;
-  }
-
-}
-
-
-// complete = portion (between 0 and 1) of the way through animation.
-function animateItem(element, item, fraction) {
-  let player = getPlayerForItem(element, item);
-  if (player) {
-    let duration = element.animationDuration;
-    let time = fraction * duration;
-    player.currentTime = time;
-  }
-}
-
-function getPlayerForItem(element, item) {
-  let players = element.players;
-  if (!players) {
-    return;
-  }
-  let index = element.items.indexOf(item);
-  if (index < 0) {
-    return null;
-  }
-  let player = players[index];
-  if (!player) {
-    console.log(`Creating player ${index}`);
-    let animation = element.animation;
-    let animationOptions = {
-      duration: element.animationDuration,
-      easing: 'ease-in-out',
-      fill: 'both'
-    };
-    player = item.animate(animation, animationOptions);
-    player.pause();
-    players[index] = player;
-  }
-  return player;
 }
 
 
--- packages/basic-component-mixins/src/SelectionAnimation.js
+++ packages/basic-component-mixins/src/SelectionAnimation.js
@@ -4,26 +4,73 @@ export default (base) => {
   /**
    * Animates selection
    *
-   * Expects: animations
+   * Expects: selectedItem, animation, animationDuration properties
    */
   class SelectionAnimation extends base {
 
-    get position() {
-      return super.position;
-    }
-    set position(index) {
-      if ('position' in base.prototype) { super.position = index; }
-    }
+    // get position() {
+    //   return super.position;
+    // }
+    // set position(index) {
+    //   if ('position' in base.prototype) { super.position = index; }
+    // }
 
-    get selectedIndex() {
-      return super.selectedIndex;
+    get selectedItem() {
+      return super.selectedItem;
     }
-    set selectedIndex(index) {
-      if ('selectedIndex' in base.prototype) {
-        super.selectedIndex = index;
-      }
+    set selectedItem(item) {
+      if ('selectedItem' in base.prototype) { super.selectedItem = item; }
+      let index = this.items.indexOf(item);
+      animateSelection(this, this._previousSelectedIndex, index);
+      this._previousSelectedIndex = index;
     }
   }
 
   return SelectionAnimation;
 };
+
+
+function animateSelection(element, fromIndex, toIndex) {
+  console.log(`animating from ${fromIndex} to ${toIndex}`);
+  let animateForward = fromIndex === null || toIndex > fromIndex;
+  if (fromIndex >= 0) {
+    element.animateItem(element.items[fromIndex], 0, animateForward ? 1 : -1);
+  }
+  element.animateItem(element.items[toIndex], animateForward ? -1 : 1, 0);
+}
+
+
+// complete = portion (between 0 and 1) of the way through animation.
+// function animateItem(element, item, fraction) {
+//   let player = getPlayerForItem(element, item);
+//   if (player) {
+//     let duration = element.animationDuration;
+//     let time = fraction * duration;
+//     player.currentTime = time;
+//   }
+// }
+//
+// function getPlayerForItem(element, item) {
+//   let players = element.players;
+//   if (!players) {
+//     return;
+//   }
+//   let index = element.items.indexOf(item);
+//   if (index < 0) {
+//     return null;
+//   }
+//   let player = players[index];
+//   if (!player) {
+//     console.log(`Creating player ${index}`);
+//     let animation = element.animation;
+//     let animationOptions = {
+//       duration: element.animationDuration,
+//       easing: 'ease-in-out',
+//       fill: 'both'
+//     };
+//     player = item.animate(animation, animationOptions);
+//     player.pause();
+//     players[index] = player;
+//   }
+//   return player;
+// }
