d0e699026cce43143744e6ba450692fb10764904
Jan Miksovsky <jan@quickui.org>
2016-05-25 11:03:45 -0700

    Belatedly rename ContentItems.tests.js -> ContentAsItems.test.js, to match the corresponding ContentAsItems.js mixin. ListBox unit test waits for items array to be populated by async contentChanged call.

--- packages/basic-list-box/test/ListBox.tests.js
+++ packages/basic-list-box/test/ListBox.tests.js
@@ -1,16 +1,20 @@
 import { assert } from 'chai';
 import ListBox from '../src/ListBox'; // jshint ignore:line
+import microtask from '../../basic-component-mixins/src/microtask';
 
 
 describe("ListBox", () => {
 
-  it("defines a value property equivalent to text of selectedItem", () => {
+  it("defines a value property equivalent to text of selectedItem", done => {
     let listBox = createSampleListBox();
-    assert.equal(listBox.value, '');
-    listBox.selectedIndex = 2;
-    assert.equal(listBox.value, 'Two');
-    listBox.value = 'One';
-    assert.equal(listBox.selectedIndex, 1);
+    microtask(() => {
+      assert.equal(listBox.value, '');
+      listBox.selectedIndex = 2;
+      assert.equal(listBox.value, 'Two');
+      listBox.value = 'One';
+      assert.equal(listBox.selectedIndex, 1);
+      done();
+    });
   });
 
 });
